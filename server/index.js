9271b9bd00d58ed3b02b3a2fc0ba4b58f9e4bfe1
Yevhenii Moskalenko <testjokerqwerty@gmail.com>
2019-08-05 01:48:59 +0300

    add new components and col el

--- server/index.js
+++ server/index.js
@@ -1,17 +1,23 @@
 const express = require('express')
 const consola = require('consola')
-const { Nuxt, Builder } = require('nuxt')
+const {
+  Nuxt,
+  Builder
+} = require('nuxt')
 const app = express()
 
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
 config.dev = process.env.NODE_ENV !== 'production'
 
-async function start () {
+async function start() {
   // Init Nuxt.js
   const nuxt = new Nuxt(config)
 
-  const { host, port } = nuxt.options.server
+  const {
+    host,
+    port
+  } = nuxt.options.server
 
   // Build only in dev mode
   if (config.dev) {
